6c2f2713091eed7cc0cf94edb6f1631f23cf5a56
Mi <mi@ss.(none)>
2014-08-17 22:37:14 +0300

    Renamed the 'Approach' to a 'Plan' in Approach form.

--- wsgi/openshift/halfmakery/forms.py
+++ wsgi/openshift/halfmakery/forms.py
@@ -12,7 +12,7 @@ class ApproachForm(forms.ModelForm):
         self.fields['category'].label = mark_safe('<span id="swap"><span id="old_idea"><u>Categories</u></span><span id="new_idea"><a href="/categories/" target="_blank">Categories</a></span></span>')
         self.fields['subcategory'].label = mark_safe('<span id="swap"><span id="old_idea"><u>Subcategories</u></span><span id="new_idea"><a href="/subcategories/" target="_blank">Subcategories</a></span></span>')
         self.fields['idea'].label = mark_safe('<span id="swap"><span id="old_idea"><u>Idea</u></span><span id="new_idea"><a href="/ideas/" target="_blank">Add New</a></span></span>')
-        self.fields['name'].label = 'Approach'
+        self.fields['name'].label = 'Plan'
         self.fields['sketch'].label = 'Situation'
 
 class MilestoneForm(forms.ModelForm):
